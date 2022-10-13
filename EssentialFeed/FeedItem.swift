ecdb1e89a6e51549df464ead672a438e34d19bae
Mohammadreza Khatibi <mohammadrezakhatibi@outlook.com>
2022-10-13 11:10:05 +0300

    Initial Commit

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by mohammadreza on 10/13/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    var id: UUID
+    var description: String?
+    var location: String?
+    var imageURL: URL
+}
