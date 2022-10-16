c25ddd272eebe011e5f89b7ef56c72572d1dc1d6
Mohammadreza Khatibi <mohammadrezakhatibi@outlook.com>
2022-10-16 22:29:40 +0300

    Remove `LoadFeedResult` Equatable conformance from production since it's a test-only requirement

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -117,18 +117,32 @@ class RemoteFeedLoaderTests: XCTestCase {
     // MARK: - Helpers
 
     private func expect(_ sut: RemoteFeedLoader,
-                        toCompleteWith result: RemoteFeedLoader.Result,
+                        toCompleteWith expectedResult: RemoteFeedLoader.Result,
                         when action: () -> Void,
                         file: StaticString = #filePath,
                         line: UInt = #line) {
         
-        var capturedResult = [RemoteFeedLoader.Result]()
+        let exp = expectation(description: "Wait for load completion")
         
-        sut.load { capturedResult.append($0) }
+        sut.load { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedItems), .success(receivedItems)):
+                XCTAssertEqual(receivedItems, receivedItems, file: file, line: line)
+                
+            case let (.failure(receivedError), .failure(expectedError)):
+                XCTAssertEqual(receivedError,expectedError, file: file, line: line)
+                
+            default:
+                XCTFail("Expected result \(expectedResult) got \(receivedResult) instead", file: file, line: line)
+                
+            }
+            exp.fulfill()
+        }
         
         action()
         
-        XCTAssertEqual(capturedResult, [result], file: file, line: line)
+        wait(for: [exp], timeout: 1)
+        
     }
     
     private func makeSUT(url: URL = URL(string: "http://a-url.com")!,
