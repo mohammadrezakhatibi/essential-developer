0905a7e699600d8052c352fe44426048ebec4816
Mohammadreza Khatibi <mohammadrezakhatibi@outlook.com>
2022-10-17 15:58:43 +0300

    Move start/stop intercepting to setUp and tradDown functions

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -26,8 +26,17 @@ class URLSessionHTTPClient {
 
 class URLSessionHTTPClientTests: XCTestCase {
     
-    func test_getFromURL_performGETRequestWithURL() {
+    override func setUp() {
+        super.setUp()
         URLProtocolStub.startInterceptingRequests()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        URLProtocolStub.stopInterceptingRequests()
+    }
+    
+    func test_getFromURL_performGETRequestWithURL() {
         
         let sut = URLSessionHTTPClient()
         
@@ -44,11 +53,9 @@ class URLSessionHTTPClientTests: XCTestCase {
         sut.get(from: url) { _ in }
         
         wait(for: [exp], timeout: 1.0)
-        URLProtocolStub.stopInterceptingRequests()
     }
     
     func test_getFromURL_failsOnRequestError() {
-        URLProtocolStub.startInterceptingRequests()
         let url = URL(string: "http://a-url.com")!
         let error = NSError(domain: "a error", code: 1)
         URLProtocolStub.stub(data: nil, response: nil, error: error)
@@ -70,7 +77,6 @@ class URLSessionHTTPClientTests: XCTestCase {
         }
     
         wait(for: [exp], timeout: 1.0)
-        URLProtocolStub.stopInterceptingRequests()
     }
     
     private class URLProtocolStub: URLProtocol {
