d147393ae2f0f4333d3a9bb633161169622600f1
Mohammadreza Khatibi <mohammadrezakhatibi@outlook.com>
2022-10-18 11:32:33 +0300

    Move `URLSessionHTTPClient` to Feed API production module

---
+++ EssentialFeed/Feed API/URLSessionHTTPClient.swift
@@ -0,0 +1,31 @@
+//
+//  URLSessionHTTPClient.swift
+//  EssentialFeed
+//
+//  Created by mohammadreza on 10/18/22.
+//
+
+import Foundation
+
+public class URLSessionHTTPClient: HTTPClient {
+    
+    private var session: URLSession
+    
+    public init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    private struct UnexpectedValueRepresentation: Error {}
+    
+    public func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+        session.dataTask(with: url) { data, response, error in
+            if let error {
+                completion(.failure(error))
+            } else if let data = data, let response = response as? HTTPURLResponse {
+                completion(.success(data, response))
+            } else {
+                completion(.failure(UnexpectedValueRepresentation()))
+            }
+        }.resume()
+    }
+}
