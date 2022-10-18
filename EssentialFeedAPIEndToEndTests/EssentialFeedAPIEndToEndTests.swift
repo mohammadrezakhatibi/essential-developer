be83a861824331fb1dda43bc8a2f64f3c993b243
Mohammadreza Khatibi <mohammadrezakhatibi@outlook.com>
2022-10-18 16:28:59 +0300

    Extract test setup to a helper method

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -11,20 +11,8 @@ import EssentialFeed
 class EssentialFeedAPIEndToEndTests: XCTestCase {
 
     func test_endToEndTestServerGetFeedResult_matchesFixedAccountData() {
-        let url = URL(string: "https://lokomond.com/essential-feed/feed.json")!
-        let client = URLSessionHTTPClient()
-        let loader = RemoteFeedLoader(url: url, client: client)
-            
-        let exp = expectation(description: "Wait for load completion")
         
-        var receivedResult: LoadFeedResult?
-        loader.load { result in
-            receivedResult = result
-            exp.fulfill()
-        }
-        
-        wait(for: [exp], timeout: 5.0)
-        switch receivedResult {
+        switch getFeedResult() {
         case let .success(items)?:
             XCTAssertEqual(items.count, 8, "Expected 8 items in the test account feed")
             XCTAssertEqual(items[0], expectedItem(at: 0))
@@ -44,11 +32,30 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
         }
     }
     
+    // MARK: - Helpers
+    
     private func expectedItem(at index: Int) -> FeedItem {
         return FeedItem(id: id(at: index),
                         description: description(at: index),
                         location: location(at: index),
                         imageURL: imageURL(at: index))
+    }
+    
+    private func getFeedResult() -> LoadFeedResult? {
+        let url = URL(string: "https://lokomond.com/essential-feed/feed.json")!
+        let client = URLSessionHTTPClient()
+        let loader = RemoteFeedLoader(url: url, client: client)
+            
+        let exp = expectation(description: "Wait for load completion")
+        
+        var receivedResult: LoadFeedResult?
+        loader.load { result in
+            receivedResult = result
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 5.0)
+        return receivedResult
     }
     
     private func id(at index: Int) -> UUID {
