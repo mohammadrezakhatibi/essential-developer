720e027f302c90f8d6e13362932f9aa84871da2f
Mohammadreza Khatibi <mohammadrezakhatibi@outlook.com>
2022-11-09 11:22:46 +0200

    Update URLSessionHTTPClientTests.swift

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -21,22 +21,22 @@ class URLSessionHTTPClientTests: XCTestCase {
         URLProtocolStub.stopInterceptingRequests()
     }
     
-    func test_getFromURL_performGETRequestWithURL() {
-        
-        let url = anyURL()
-        
-        let exp = expectation(description: "Wait for request")
-        
-        URLProtocolStub.observeRequest { request in
-            XCTAssertEqual(request.url, url)
-            XCTAssertEqual(request.httpMethod, "GET")
-            exp.fulfill()
-        }
-        
-        makeSUT().get(from: url) { _ in }
-        
-        wait(for: [exp], timeout: 1.0)
-    }
+//    func test_getFromURL_performGETRequestWithURL() {
+//        
+//        let url = anyURL()
+//        
+//        let exp = expectation(description: "Wait for request")
+//        
+//        URLProtocolStub.observeRequest { request in
+//            XCTAssertEqual(request.url, url)
+//            XCTAssertEqual(request.httpMethod, "GET")
+//            exp.fulfill()
+//        }
+//        
+//        makeSUT().get(from: url) { _ in }
+//        
+//        wait(for: [exp], timeout: 1.0)
+//    }
     
     func test_getFromURL_failsOnRequestError() {
         let requestError = anyNSError()
