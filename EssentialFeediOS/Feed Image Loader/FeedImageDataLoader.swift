bc36dcd0e725f224242ea7d5de30fc6baa0f9514
Mohammadreza Khatibi <mohammadrezakhatibi@outlook.com>
2022-11-12 19:10:55 +0200

    Move production codes to theri own files

---
+++ EssentialFeediOS/Feed Image Loader/FeedImageDataLoader.swift
@@ -0,0 +1,16 @@
+//
+//  FeedImageDataLoader.swift
+//  EssentialFeediOS
+//
+//  Created by Mohammadreza on 11/12/22.
+//
+
+import Foundation
+
+public protocol FeedImageDataLoaderTask {
+    func cancel()
+}
+public protocol FeedImageDataLoader {
+    typealias Result = Swift.Result<Data, Error>
+    func loadImageData(from url: URL, completion: @escaping (Result) -> Void) -> FeedImageDataLoaderTask
+}
