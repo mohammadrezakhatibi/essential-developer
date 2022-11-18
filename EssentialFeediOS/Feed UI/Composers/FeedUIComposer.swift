39fd0ab7945d1c38cb03aee37efed1fe71509942
Mohammadreza Khatibi <mohammadrezakhatibi@outlook.com>
2022-11-18 14:57:40 +0200

    Add Presentable View Models as pure data to clarify communication between Presentation and UI.

--- EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -35,8 +35,8 @@ private final class WeakRefVirtualProxy<T: AnyObject> {
 
 extension WeakRefVirtualProxy: FeedLoadingView where T: FeedLoadingView {
     
-    func display(isLoading: Bool) {
-        object?.display(isLoading: isLoading)
+    func display(_ viewModel: FeedLoadingViewModel) {
+        object?.display(viewModel)
     }
 }
 
@@ -48,8 +48,8 @@ private final class FeedViewAdapter: FeedView {
         self.controller = controller
         self.loader = loader
     }
-    func display(feed: [FeedImage]) {
-        controller?.tableModel = feed.map{ model in
+    func display(_ viewModel: FeedViewModel) {
+        controller?.tableModel = viewModel.feed.map{ model in
             let viewModel = FeedImageViewModel(model: model, imageLoader: loader, imageTransformer: UIImage.init)
             return FeedImageCellController(viewModel: viewModel)
         }
