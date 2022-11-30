f257989f9cf6922c1022b7454a40aab580978a32
Mohammadreza Khatibi <mohammadrezakhatibi@outlook.com>
2022-11-30 14:05:11 +0200

    Create `EssentialApp` workspace combining `EssentialApp` and `EssentialFeed` projects

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by mohammadreza on 11/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
