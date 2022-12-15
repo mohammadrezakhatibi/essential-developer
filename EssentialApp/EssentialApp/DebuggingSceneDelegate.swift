d6c158dd3cb907eb6c032aec8af16c73eb94f6b5
Mohammadreza Khatibi <mohammadrezakhatibi@outlook.com>
2022-12-15 22:55:57 +0200

    Intercept HTTP requests with canned responses during UI tests to eliminate network flakiness - We can now run UI tests without internet connection.

--- EssentialApp/EssentialApp/DebuggingSceneDelegate.swift
+++ EssentialApp/EssentialApp/DebuggingSceneDelegate.swift
@@ -22,22 +22,70 @@ class DebuggingSceneDelegate: SceneDelegate {
     }
     
     override func makeRemoteClient() -> HTTPClient {
-        if UserDefaults.standard.string(forKey: "connectivity") == "offline" {
-            return AlwaysFailingHTTPClient()
+        if let connectivity = UserDefaults.standard.string(forKey: "connectivity") {
+            return DebuggingHTTPClient(connectivity: connectivity)
         }
         
         return super.makeRemoteClient()
     }
 }
 
-private final class AlwaysFailingHTTPClient: HTTPClient {
+private final class DebuggingHTTPClient: HTTPClient {
     private class Task: HTTPClientTask {
         func cancel() { }
     }
     
+    var connectivity: String
+    
+    init(connectivity: String) {
+        self.connectivity = connectivity
+    }
+    
     func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
-        completion(.failure(NSError(domain: "offline", code: 0)))
+        switch connectivity {
+        case "online":
+            completion(.success(makeSuccessfulResponse(for: url)))
+            
+        default:
+            completion(.failure(NSError(domain: "offline", code: 0)))
+        }
         return Task()
+    }
+    
+    private func makeSuccessfulResponse(for url: URL) -> (Data, HTTPURLResponse) {
+        let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)!
+        return (makeData(for: url), response)
+    }
+    
+    private func makeData(for url: URL) -> Data {
+        switch url.absoluteString {
+        case "http://image.con":
+            return makeImageData()
+        
+        default:
+            return makeFeedData()
+        }
+        
+    }
+    
+    private func makeImageData() -> Data {
+        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
+        let format = UIGraphicsImageRendererFormat()
+        format.scale = 1
+        
+        let image = UIGraphicsImageRenderer(bounds: rect, format: format).image { rendererContext in
+            let color = UIColor.red
+            color.setFill()
+            rendererContext.fill(rect)
+        }
+        return image.pngData()!
+    }
+    
+    private func makeFeedData() -> Data {
+        return try! JSONSerialization.data(withJSONObject: ["items" : [
+            ["id": UUID().uuidString, "image": "http://image.com"],
+            ["id": UUID().uuidString, "image": "http://image.com"],
+        ]])
     }
 }
 #endif
--- EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -11,11 +11,11 @@ final class EssentialAppUIAcceptanceTests: XCTestCase {
 
     func test_onLaunch_displaysRemoteFeedWhenCustomersHasConnectivity() {
         let app = XCUIApplication()
-        app.launchArguments = ["-reset"]
+        app.launchArguments = ["-reset", "-connectivity", "online"]
         app.launch()
         
         let feedCells = app.cells.matching(identifier: "feed-image-cell")
-        XCTAssertEqual(feedCells.count, 22)
+        XCTAssertEqual(feedCells.count, 2)
         
         let firstImage = app.images.matching(identifier: "feed-image-view").firstMatch
         XCTAssertTrue(firstImage.exists)
@@ -23,7 +23,7 @@ final class EssentialAppUIAcceptanceTests: XCTestCase {
     
     func test_onLaunch_displaysCachedRemoteFeedWhenCustomersHasNoConnectivity() {
         let onlineApp = XCUIApplication()
-        onlineApp.launchArguments = ["-reset"]
+        onlineApp.launchArguments = ["-reset", "-connectivity", "online"]
         onlineApp.launch()
         
         let offlineApp = XCUIApplication()
@@ -31,7 +31,7 @@ final class EssentialAppUIAcceptanceTests: XCTestCase {
         offlineApp.launch()
         
         let cachedFeedCell = offlineApp.cells.matching(identifier: "feed-image-cell")
-        XCTAssertEqual(cachedFeedCell.count, 22)
+        XCTAssertEqual(cachedFeedCell.count, 2)
         
         let firstCachedImage = offlineApp.images.matching(identifier: "feed-image-view").firstMatch
         XCTAssertTrue(firstCachedImage.exists)
