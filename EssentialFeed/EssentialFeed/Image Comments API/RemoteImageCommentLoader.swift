8a956b9b4dcaa0ed47868d1a45f91db522598138
Mohammadreza Khatibi <mohammadrezakhatibi@outlook.com>
2023-01-09 23:33:23 +0200

    Replace RemoteImageCommentLoader with the generic RemoteLoader to remove duplication

--- EssentialFeed/EssentialFeed/Image Comments API/RemoteImageCommentLoader.swift
+++ EssentialFeed/EssentialFeed/Image Comments API/RemoteImageCommentLoader.swift
@@ -7,44 +7,10 @@
 
 import Foundation
 
-public final class RemoteImageCommentLoader {
-    
-    private var url: URL
-    private var client: HTTPClient
-    
-    public enum Error: Swift.Error {
-        case connectivity
-        case invalidData
-    }
-    
-    public typealias Result = Swift.Result<[ImageComment], Swift.Error>
-    
-    public init(url: URL, client: HTTPClient) {
-        self.url = url
-        self.client = client
-    }
-    
-    public func load(completion: @escaping (Result) -> Void) {
-        client.get(from: url) { [weak self] result in
-            guard self != nil else { return }
-            switch result {
-            case let .success((data, response)):
-                completion(RemoteImageCommentLoader.map(data, response: response))
-                
-            case .failure:
-                completion(.failure(RemoteImageCommentLoader.Error.connectivity))
-            }
-        }
-    }
-    
-    private static func map(_ data: Data, response: HTTPURLResponse) -> Result {
-        
-        do {
-            let items = try ImageCommentMapper.map(data, response: response)
-            return .success(items)
-        } catch {
-            return .failure(error)
-        }
-        
+public typealias RemoteImageCommentLoader = RemoteLoader<[ImageComment]>
+
+public extension RemoteImageCommentLoader {
+    convenience init(url: URL, client: HTTPClient) {
+        self.init(url: url, client: client, mapper: ImageCommentMapper.map)
     }
 }
--- EssentialFeed/EssentialFeedTests/Image Comments API/LoadImageCommentsFromRemoteUseCase.swift
+++ EssentialFeed/EssentialFeedTests/Image Comments API/LoadImageCommentsFromRemoteUseCase.swift
@@ -10,41 +10,6 @@ import EssentialFeed
 
 final class LoadImageCommentsFromRemoteUseCase: XCTestCase {
     
-    func test_init_doesNotRequestDataFromURL() {
-        let url = URL(string: "http://a-url.com")!
-        let (_, client) = makeSUT(url: url)
-        
-        XCTAssertTrue(client.requestedURLs.isEmpty)
-    }
-    
-    func test_load_requestsDataFromURL() {
-        let url = URL(string: "http://a-url.com")!
-        let (sut, client) = makeSUT(url: url)
-        
-        sut.load { _ in }
-        
-        XCTAssertEqual(client.requestedURLs, [url])
-    }
-    
-    func test_loadTwice_requestsDataFromURLTwice() {
-        let url = URL(string: "http://a-url.com")!
-        let (sut, client) = makeSUT(url: url)
-        
-        sut.load { _ in }
-        sut.load { _ in }
-        
-        XCTAssertEqual(client.requestedURLs, [url, url])
-    }
-    
-    func test_load_deliversErrorOnClientError() {
-        let (sut, client) = makeSUT()
-        
-        expect(sut, toCompleteWith: failure(.connectivity), when: {
-            let clientError = NSError(domain: "Test", code: 0)
-            client.complete(with: clientError)
-        })
-    }
-    
     func test_load_deliversErrorOnNon2xxHTTPResponse() {
         let (sut, client) = makeSUT()
         
@@ -113,21 +78,6 @@ final class LoadImageCommentsFromRemoteUseCase: XCTestCase {
         
     }
     
-    func test_load_doesNotDeliverResultAfterSUTInstanceHasBeenDeallocated() {
-        let url = URL(string: "http://a-url.com")!
-        let client = HTTPClientSpy()
-        var sut: RemoteImageCommentLoader? = RemoteImageCommentLoader(url: url, client: client)
-        
-        var capturedResult = [RemoteImageCommentLoader.Result]()
-        
-        sut?.load { capturedResult.append($0) }
-        
-        sut = nil
-        client.complete(withStatusCode: 200, data: makeItemsJSON([]))
-        
-        XCTAssertTrue(capturedResult.isEmpty)
-    }
-                
     // MARK: - Helpers
 
     private func expect(_ sut: RemoteImageCommentLoader,
