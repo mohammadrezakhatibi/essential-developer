aab3ed78823e8d40b0438bbca156e94ac44b2118
Mohammadreza Khatibi <mohammadrezakhatibi@outlook.com>
2023-02-09 22:51:17 +0200

    Update endpoints

--- EssentialFeed/EssentialFeed/Feed API/ImageFeedEndpoint.swift
+++ EssentialFeed/EssentialFeed/Feed API/ImageFeedEndpoint.swift
@@ -6,11 +6,7 @@ public enum ImageFeedEndpoint {
     public func url(baseURL: URL) -> URL {
         switch self {
             case .get:
-                if #available(iOS 13, *) {
-                    return baseURL.appending(path: "/v1/feed")
-                } else {
-                    return baseURL.appendingPathExtension("/v1/feed")
-                }
+                return baseURL.appendingPathComponent("/v1/feed")
         }
     }
 }
--- EssentialFeed/EssentialFeed/Image Comments API/ImageCommentEndpoint.swift
+++ EssentialFeed/EssentialFeed/Image Comments API/ImageCommentEndpoint.swift
@@ -6,11 +6,7 @@ public enum ImageCommentsEndPoint {
     public func url(baseURL: URL) -> URL {
         switch self {
             case let .get(id):
-                if #available(iOS 13, *) {
-                    return baseURL.appending(path: "/v1/image/\(id)/comments")
-                } else {
-                    return baseURL.appendingPathExtension("/v1/image/\(id)/comments")
-                }
+                return baseURL.appendingPathComponent("/v1/image/\(id)/comments")
         }
     }
 }
