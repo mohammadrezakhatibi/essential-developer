55a86e3d553bab7617a29d38b2566befca92eef2
Mohammadreza Khatibi <mohammadrezakhatibi@outlook.com>
2023-02-11 18:42:53 +0200

    Make tests pass

--- EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -47,8 +47,9 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
         let client = ephemeralClient()
 		let exp = expectation(description: "Wait for load completion")
 		
+        let url = feedTestServerURL.appendingPathComponent("feed.json")
 		var receivedResult: Swift.Result<[FeedImage], Error>?
-        client.get(from: feedTestServerURL) { result in
+        client.get(from: url) { result in
             receivedResult = result.flatMap { (data, response) in
                 do {
                     return .success(try FeedItemsMapper.map(data, from: response))
@@ -65,7 +66,7 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
 	
 	private func getFeedImageDataResult(file: StaticString = #filePath, line: UInt = #line) -> FeedImageDataLoader.Result? {
 		let client = ephemeralClient()
-        let url = feedTestServerURL.appendingPathComponent("73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6/image")
+        let url = feedTestServerURL.appendingPathComponent("swift.png")
         let exp = expectation(description: "Wait for load completion")
 
 		var receivedResult: FeedImageDataLoader.Result?
@@ -85,7 +86,7 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
 	}
 	
 	private var feedTestServerURL: URL {
-		return URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
+		return URL(string: "https://lokomond.com/essential-feed/")!
 	}
 	
 	private func ephemeralClient(file: StaticString = #filePath, line: UInt = #line) -> HTTPClient {
@@ -101,47 +102,47 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
 			location: location(at: index),
 			url: imageURL(at: index))
 	}
-	
-	private func id(at index: Int) -> UUID {
-		return UUID(uuidString: [
-			"73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6",
-			"BA298A85-6275-48D3-8315-9C8F7C1CD109",
-			"5A0D45B3-8E26-4385-8C5D-213E160A5E3C",
-			"FF0ECFE2-2879-403F-8DBE-A83B4010B340",
-			"DC97EF5E-2CC9-4905-A8AD-3C351C311001",
-			"557D87F1-25D3-4D77-82E9-364B2ED9CB30",
-			"A83284EF-C2DF-415D-AB73-2A9B8B04950B",
-			"F79BD7F8-063F-46E2-8147-A67635C3BB01"
-		][index])!
-	}
-	
-	private func description(at index: Int) -> String? {
-		return [
-			"Description 1",
-			nil,
-			"Description 3",
-			nil,
-			"Description 5",
-			"Description 6",
-			"Description 7",
-			"Description 8"
-		][index]
-	}
-	
-	private func location(at index: Int) -> String? {
-		return [
-			"Location 1",
-			"Location 2",
-			nil,
-			nil,
-			"Location 5",
-			"Location 6",
-			"Location 7",
-			"Location 8"
-		][index]
-	}
-	
-	private func imageURL(at index: Int) -> URL {
-		return URL(string: "https://url-\(index+1).com")!
-	}
+
+    private func id(at index: Int) -> UUID {
+        return UUID(uuidString: [
+            "73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6",
+            "BA298A85-6275-48D3-8315-9C8F7C1CD109",
+            "5A0D45B3-8E26-4385-8C5D-213E160A5E3C",
+            "FF0ECFE2-2879-403F-8DBE-A83B4010B340",
+            "DC97EF5E-2CC9-4905-A8AD-3C351C311001",
+            "557D87F1-25D3-4D77-82E9-364B2ED9CB30",
+            "A83284EF-C2DF-415D-AB73-2A9B8B04950B",
+            "F79BD7F8-063F-46E2-8147-A67635C3BB01"
+        ][index])!
+    }
+
+    private func description(at index: Int) -> String? {
+        return [
+            "Description 1",
+            nil,
+            "Description 3",
+            nil,
+            "Description 5",
+            "Description 6",
+            "Description 7",
+            "Description 8"
+        ][index]
+    }
+
+    private func location(at index: Int) -> String? {
+        return [
+            "Location 1",
+            "Location 2",
+            nil,
+            nil,
+            "Location 5",
+            "Location 6",
+            "Location 7",
+            "Location 8"
+        ][index]
+    }
+
+    private func imageURL(at index: Int) -> URL {
+        return URL(string: "https://url-\(index+1).com")!
+    }
 }
