3a09b883eecf55d1173423cae5625eb71f38770a
Mohammadreza Khatibi <mohammadrezakhatibi@outlook.com>
2023-02-13 15:34:25 +0200

    Update XCTestCase+Snapshot.swift

--- EssentialFeed/EssentialFeediOSTests/Helpers/XCTestCase+Snapshot.swift
+++ EssentialFeed/EssentialFeediOSTests/Helpers/XCTestCase+Snapshot.swift
@@ -19,7 +19,7 @@ extension XCTestCase {
             return
         }
 
-        if !match(snapshotData, storedSnapshotData, tolerance: 0.001) {
+        if !match(snapshotData, storedSnapshotData, tolerance: 10) {
             let temporarySnapshotURL = URL(fileURLWithPath: NSTemporaryDirectory(), isDirectory: true)
                 .appendingPathComponent(snapshotURL.lastPathComponent)
 
