ef7808fd40b49c654b425739aa3e4b8193ff465e
Mohammadreza Khatibi <mohammadrezakhatibi@outlook.com>
2023-02-16 11:48:50 +0200

    Save images synchronously

--- EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedImageDataStoreSpy.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedImageDataStoreSpy.swift
@@ -13,11 +13,11 @@ class FeedImageDataStoreSpy: FeedImageDataStore {
 	
 	private(set) var receivedMessages = [Message]()
 	private var retrievalCompletions = [(FeedImageDataStore.RetrievalResult) -> Void]()
-	private var insertionCompletions = [(FeedImageDataStore.InsertionResult) -> Void]()
+    private var insertionResult: Result<Void, Error>?
 
-	func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
-		receivedMessages.append(.insert(data: data, for: url))
-		insertionCompletions.append(completion)
+    func insert(_ data: Data, for url: URL) throws {
+        receivedMessages.append(.insert(data: data, for: url))
+        try insertionResult?.get()
 	}
 	
 	func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
@@ -34,10 +34,10 @@ class FeedImageDataStoreSpy: FeedImageDataStore {
 	}
 	
 	func completeInsertion(with error: Error, at index: Int = 0) {
-		insertionCompletions[index](.failure(error))
+		insertionResult = .failure(error)
 	}
 	
 	func completeInsertionSuccessfully(at index: Int = 0) {
-		insertionCompletions[index](.success(()))
+		insertionResult = .success(())
 	}
 }
